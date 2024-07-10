29edddc64d8c1a8e63f3b557f415bc7091234837
cisco <cisco@UPOAULA10318>
2024-07-10 10:43:53 -0500

    Agregar archivos de proyecto.

---
+++ CruzEvaluacion3P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CruzEvaluacion3P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CruzEvaluacion3P/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CruzEvaluacion3P
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
